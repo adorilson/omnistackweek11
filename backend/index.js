3ed13ba3a41474342ddd760282e922c6b91a5c1b
Adorilson Bezerra <adorilson@gmail.com>
2020-03-24 19:23:16 -0300

    Understanding methods and params HTTP
    2nd class, until around 23m15

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,40 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+// necessário para a aplicação entender json no corpo das requisicoes
+app.use(express.json());
+
+/**
+ * Rota / Recurso 
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET: buscar uma informação do back-end
+  * POST: criar uma informação no back-end
+  * PUT: alterar uma informação no back-end
+  * DELETE: deletar uma informação no back-end
+  */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: parametros nomeados enviados na rota após ? (filtros, paginação)
+ *      acessados com request.query
+ * Route Params: parâmetros utilizados para identificar recursos
+ *      acessados com request.params
+ * Request Body: corpo da requisicao, utilizado para criar ou alterar recursos
+ *      acessados com request.body
+ *  
+ */
+
+app.post('/users', (request, response) => {
+
+    const params = request.body;
+
+    console.log(params);
+
     return response.json({
         evento: 'Semana OmniStack 11.0',
         aluno: 'Adorilson Bezerra'
