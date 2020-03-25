6a9c3903d8e3f2feca859b98bb5d39212a916f63
Adorilson Bezerra <adorilson@gmail.com>
2020-03-25 17:24:41 -0300

    Sending ong together yours incidents
    2nd class. ~1h33

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -21,9 +21,17 @@ module.exports = {
         const [count] = await connection('incidents').count();
 
         const data = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select('*');
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf',
+            ]);
 
         response.header('X-Total-Count', count['count(*)']);
 
