133c96d9869b9831e7a649c5457664b503c84490
Adorilson Bezerra <adorilson@gmail.com>
2020-03-26 20:29:29 -0300

    Retrieving ong name from local storage in profile page
    3rd class. ~1h28m

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -5,15 +5,15 @@ import {FiPower, FiTrash2} from 'react-icons/fi';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
-import heroesImg from '../../assets/heroes.png';
-
 
 export default function Profile(){
+    const ongName =  localStorage.getItem('ongName');
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
-                <span>Bem vinda, APAD</span>
+                <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
                 <button type="button">
