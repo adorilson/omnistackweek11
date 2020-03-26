58916e598970f13fd355675b79c3bd90cea95cfa
Adorilson Bezerra <adorilson@gmail.com>
2020-03-26 14:13:49 -0300

    Add profile ong page (just frontend, static)
    3rd class. ~1h9m

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPower, FiTrash2} from 'react-icons/fi';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+
+export default function Profile(){
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem vinda, APAD</span>
+
+                <Link className="button" to="/incidentes/new">Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower size={18} color="#E02041" />
+                </button>
+            </header>
+
+            <h1>Casos Cadastrados</h1>
+
+            <ul>
+                <li>
+                        <strong>CASO:</strong>
+                        <p>Caso Teste</p>
+
+                        <strong>DESCRICAO:</strong>
+                        <p>Descição Teste</p>
+
+                        <strong>VALOR:</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20} color="#a8a8b3"/>
+                        </button>
+
+                </li>
+
+                <li>
+                        <strong>CASO:</strong>
+                        <p>Caso Teste</p>
+
+                        <strong>DESCRICAO:</strong>
+                        <p>Descição Teste</p>
+
+                        <strong>VALOR:</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20} color="#a8a8b3"/>
+                        </button>
+
+                </li>
+
+                <li>
+                        <strong>CASO:</strong>
+                        <p>Caso Teste</p>
+
+                        <strong>DESCRICAO:</strong>
+                        <p>Descição Teste</p>
+
+                        <strong>VALOR:</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20} color="#a8a8b3"/>
+                        </button>
+
+                </li>
+
+                <li>
+                        <strong>CASO:</strong>
+                        <p>Caso Teste</p>
+
+                        <strong>DESCRICAO:</strong>
+                        <p>Descição Teste</p>
+
+                        <strong>VALOR:</strong>
+                        <p>R$ 120,00</p>
+
+                        <button type="button">
+                            <FiTrash2 size={20} color="#a8a8b3"/>
+                        </button>
+
+                </li>
+            </ul>
+        </div>
+    )
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,8 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+
 
 export default function Routes(){
     return (
@@ -11,6 +13,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
     )
