7b52ace0ed2acbfed3e917c8d79e8773144b1317
Adorilson Bezerra <adorilson@gmail.com>
2020-03-25 23:17:51 -0300

    Created logon form
    3rd class. ~43m

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
 export default function Logon(){
     return (
-        <h1>Hello World Logon</h1>
+       <div className="logon-container">
+           <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="Sua ID" />
+                    <button className="button" type="Submit">Entrar</button>
+
+                    <a href="/register" >
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho cadastro
+                    </a>
+                </form>
+           </section>
+           <img src={heroesImg} alt="Heroes"/>
+       </div>
     )
 }
