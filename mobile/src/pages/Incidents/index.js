102e956d9a67d41a98155061167e34101c7b2fbf
Adorilson Bezerra <adorilson@gmail.com>
2020-03-27 14:08:26 -0300

    Started incidents page layout
    4th class. ~39m

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, Image} from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Incidents(){
     return (
-        <View />
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos</Text>
+                </Text>
+            </View>
+
+            <Text style={styles.title}>Bem-Vindo!</Text>
+            <Text style={styles.description}>
+                Escolha um dos casos abaixo e ajude a salvar o dia.
+            </Text>
+
+        </View>
     );
 }
