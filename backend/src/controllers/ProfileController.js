cfdb573a70289dadb625bd4d168962fabfaba32f
Adorilson Bezerra <adorilson@gmail.com>
2020-03-26 21:50:06 -0300

    Listing incidents in ong profile page
    3rd class. ~1h35m

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -7,7 +7,7 @@ module.exports = {
         const incidents = await connection('incidents')
             .where('ong_id', ong_id)
             .select('*');
-        
-            return response.json({incidents})
+
+            return response.json(incidents);
     }
 }
