e5665eb3147829ee725bb705eef82fe16ffd8dd7
Adorilson Bezerra <adorilson@gmail.com>
2020-03-26 22:12:18 -0300

    Implementing logout button
    3rd class. ~1h40m

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import {FiPower, FiTrash2} from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -9,6 +9,8 @@ import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile(){
+    const history = useHistory();
+
     const [incidents, setIncidents] = useState([]);
 
     const ongId = localStorage.getItem('ongId');
@@ -38,6 +40,11 @@ export default function Profile(){
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -45,7 +52,7 @@ export default function Profile(){
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
