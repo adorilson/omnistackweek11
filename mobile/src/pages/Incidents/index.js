a1c8cd8bf191c8c2c7a80797dbe4acf0d3759fb3
Adorilson Bezerra <adorilson@gmail.com>
2020-03-27 18:48:45 -0300

    Started navigation to incident detail page
    4th class. ~50m

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Feather} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
 import {View, FlatList, Text, Image, TouchableOpacity} from 'react-native';
 
 import logoImg from '../../assets/logo.png';
@@ -7,6 +8,12 @@ import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Incidents(){
+    const navigation = useNavigation();
+
+    function navigationToDetail(){
+        navigation.navigate('Detail');
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -40,7 +47,7 @@ export default function Incidents(){
 
                         <TouchableOpacity
                             style={styles.detailsButton}
-                            onPress={() => {}}>
+                            onPress={navigationToDetail}>
                                 <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                                 <Feather name='arrow-right' size={16} color='#E02041'/>
                         </TouchableOpacity>
