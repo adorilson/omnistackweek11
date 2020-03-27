93a7c86bf9cdfc9ef688b2ff09f07d331dabc44b
Adorilson Bezerra <adorilson@gmail.com>
2020-03-27 12:23:59 -0300

    Started the routes
    4th class. ~31m

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Be The Hero!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
