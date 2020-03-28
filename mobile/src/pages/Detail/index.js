2c8f4cae5d5c06e31fb43f8aee36b2df41afd03f
Adorilson Bezerra <adorilson@gmail.com>
2020-03-28 10:46:53 -0300

    Started the support to compose a whatsapp message feature (just static)
    In this moment, this  feature open the whatsapp app with
    just a static template message
    
    4th class. ~1h06m

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Feather} from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native';
-import {View, Image, Text, TouchableOpacity} from 'react-native';
+import {View, Image, Text, TouchableOpacity, Linking} from 'react-native';
 import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
@@ -19,6 +19,11 @@ export default function Detail(){
             recipients: ['contato@apad.org.br'],
             body: message,
         });
+    }
+
+    function sendWhatsapp(){
+        const toPhoneNumber = 'put a phone number';
+        Linking.openURL(`whatsapp://send?phone=${toPhoneNumber}&text=${message}`);
     }
 
     return (
@@ -47,7 +52,7 @@ export default function Detail(){
                     <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                     <View style={styles.actions}>
-                        <TouchableOpacity style={styles.action} onPress={()=>{}}>
+                        <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                             <Text style={styles.actionText}>Whatsapp</Text>
                         </TouchableOpacity>
 
