b51352106facae1ec5c036a5f223abc61b0ee05d
Adorilson Bezerra <adorilson@gmail.com>
2020-03-28 17:21:09 -0300

    Create a single unit test with jest
    5th class. ~36m

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,3 +1,5 @@
+const generateUniqueId = require('../utils/generateUniqueid');
+
 const crypto = require('crypto');
 const connection = require('../database/connection');
 
@@ -5,7 +7,7 @@ module.exports = {
     async create(request, response){
         const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
@@ -20,11 +22,11 @@ module.exports = {
 
         return response.json({id});
     },
-    
+
 
     async index(request, response){
         const ongs = await connection('ongs').select('*');
-    
-        return response.json(ongs);    
+
+        return response.json(ongs);
     }
 }
